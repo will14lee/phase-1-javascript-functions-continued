e11840e9192e1e393de8b7086271cf533e822b24
will14lee <will14lee@gmail.com>
2021-06-08 21:07:53 -0400

    commit tree 937ee56c965ddae079523d5c9f4502bdc5d47e42

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+//console.log(saturdayFun("bathe my dog"));
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork= function(activity="go to the office"){
+  console.log(`This Monday, I will ${activity}.`);
+    return `This Monday, I will ${activity}.`;
+}
+//mondayWork();
+function wrapAdjective(flair="*"){
+  return function(adj="special"){
+      console.log(`You are ${flair}${adj}${flair}!`);
+      return `You are ${flair}${adj}${flair}!`;
+  }
+}
+//const encouragingPromptFunction = wrapAdjective("!!!")()
+//wrapAdjective("%")("a dedicated programmer")
+
+
